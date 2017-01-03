cf09cea7942a6a406a10728ed00414e3d5ab8016
Pablo Henrique Penha Silva <pablohpsilva@Pablos-Mac-mini.local>
2017-01-03 08:46:41 -0200

    correcoes dos pacotes sass

--- template/src/App.vue
+++ template/src/App.vue
@@ -13,19 +13,13 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+  @import '../node_modules/vue-material/dist/vue-material.css';
+  
   .slide-fade-enter-active {
-    transition: all .3s ease;
+    transition: all .2s ease;
   }
   .slide-fade-leave-active {
-    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
   }
   .slide-fade-enter, .slide-fade-leave-active {
     transform: translateX(10px);
